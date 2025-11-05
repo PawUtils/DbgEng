a7936d5ff492cd768ca8bdb317779a14a6a0784d
Paul Chen <lancevdance@gmail.com>
2025-11-05 08:59:12 +0800

    Add global usings for native primitive types.

---
+++ Interop.DbgEng/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿// See https://learn.microsoft.com/en-us/windows/win32/winprog/windows-data-types
+
+global using BYTE = System.Byte;
+global using WORD = System.UInt16;
+global using DWORD = System.UInt32;
+global using ULONG = System.UInt32;
+global using ULONGLONG = System.UInt64;
+global using LONG = System.Int32;
+global using LONGLONG = System.Int64;
