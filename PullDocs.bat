2246e7ca92886c10957ccaaa0869fb47566865b7
Paul Chen <lancevdance@gmail.com>
2025-11-28 16:22:34 +0800

    Fix unfinished edits.

--- PullDocs.bat
+++ PullDocs.bat
@@ -9,13 +9,14 @@ IF NOT EXIST "MicrosoftDocs" (
     git clone --filter=tree:0 --sparse https://github.com/MicrosoftDocs/windows-driver-docs-ddi wdk
     pushd wdk
     git sparse-checkout set wdk-ddi-src/content/dbgeng
-    git switch --detach %rev%
+    git switch --detach %wdk_rev%
 
     popd
 
     git clone --filter=tree:0 --sparse https://github.com/MicrosoftDocs/sdk-api sdk
     pushd sdk
     git sparse-checkout set sdk-api-src/content/winnt
+    git switch --detach %sdk_rev%
 
 ) ELSE (
 
